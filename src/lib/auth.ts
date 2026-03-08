8c0dbb49ff58655c9d62e8cae880de542f9349a4
nashvin-nidu <nashvinpp2005@gmail.com>
2026-03-08 19:45:24 +0530

    feat: Implement user authentication using better-auth API routes, and setup documentatio.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,10 +2,19 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
 import { PrismaClient } from "../../generated/prisma/client";
+import { nextCookies } from "better-auth/next-js";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
+    emailAndPassword: {
+        enabled: true,
+    },
+    baseURL: process.env.BETTER_AUTH_URL,
+    secret: process.env.AUTH_SECRET,
+    debug: true,
+    trustedOrigins: [`${process.env.BETTER_AUTH_URL}`],
+    plugins: [nextCookies()],
 });
