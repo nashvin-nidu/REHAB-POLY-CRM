54baeafa62b1bbc74ce24dd50352fc458b8d4c0c
nashvin-nidu <nashvinpp2005@gmail.com>
2026-05-08 01:14:59 +0530

    feat: initialize Better Auth configuration with Prisma adapter and admin plugin

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,5 @@
 import { betterAuth } from "better-auth";
-import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prismaAdapter } from "@better-auth/prisma-adapter";
 // If your Prisma file is located elsewhere, you can change the path
 import { nextCookies } from "better-auth/next-js";
 import { admin } from "better-auth/plugins";
